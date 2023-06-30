dc2801eab594e929516dfacf0a4b52fbabd14b08
Olivera Radovanović <90214731+Olivera2708@users.noreply.github.com>
2023-07-01 01:58:10 +0200

    partial fix

--- SIMS_Projekat_Dijagram_Neaktivnosti/src/repozitorijumi/Repo.java
+++ SIMS_Projekat_Dijagram_Neaktivnosti/src/repozitorijumi/Repo.java
@@ -15,20 +15,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
 import model.Clan;
 
-public class ClanRepo {
-	
-	List<Clan> clanovi = new ArrayList<Clan>();
-	
-	public ClanRepo() throws IOException {
-		ucitajClanove();
-	}
-	
-	public void sacuvajClanove() throws IOException {
-		File fajlClanovi = new File("./podaci/clanovi.json");
+public class Repo {
+
+	public void sacuvajNaloge(List<Clan> nalozi) throws IOException {
+		File fajlNalozi = new File("./podaci/clanovi.json");
 		
-		OutputStream osNalozi = new BufferedOutputStream(new FileOutputStream(fajlClanovi));
+		OutputStream osNalozi = new BufferedOutputStream(new FileOutputStream(fajlNalozi));
 		try {
 			ObjectMapper mapper = new ObjectMapper();
 			mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
@@ -36,7 +31,7 @@ public class ClanRepo {
 			//mapper.setVisibilityChecker(mapper.getSerializationConfig().getDefaultVisibilityChecker().withFieldVisibility(Visibility.ANY));
 			mapper.enable(SerializationFeature.INDENT_OUTPUT);
 			
-			mapper.writeValue(fajlClanovi, clanovi);
+			mapper.writeValue(fajlNalozi, nalozi);
 
 			
 		} finally {
@@ -44,26 +39,30 @@ public class ClanRepo {
 		}
 	}
 	
-	public void ucitajClanove() throws IOException {
-		File fajlClanovi = new File("./podaci/clanovi.json");
+	public List<Clan> ucitajNaloge() throws IOException {
+		List<Clan> nalozi = new ArrayList<Clan>();
 		
-		InputStream isNalozi = new BufferedInputStream(new FileInputStream(fajlClanovi));
+		File fajlNalozi = new File("./podaci/clanovi.json");
+		
+		InputStream isNalozi = new BufferedInputStream(new FileInputStream(fajlNalozi));
 		
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		
 		try {
-			clanovi = mapper.readValue(fajlClanovi, new TypeReference<List<Clan>>() {});
-		} 
-		catch (Exception e) {
-			clanovi = new ArrayList<Clan>();
-		}
-		finally {
+			nalozi = mapper.readValue(fajlNalozi, new TypeReference<List<Clan>>() {});
+			
+			
+		} finally {
 			isNalozi.close();
-		}		
+		}
+		return nalozi;			
 	}
 	
-	public void dodajClana(Clan clan) throws IOException {
-		clanovi.add(clan);
-		sacuvajClanove();
+	public void dodajNalog(Clan nalog) throws IOException {
+		List<Clan> nalozi = ucitajNaloge();
+		nalozi.add(nalog);
+		sacuvajNaloge(nalozi);
 	}
+	
 }
